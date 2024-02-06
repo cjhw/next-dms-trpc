8cb6a80ce0493ad6a58d6f0535dd4b87c3d70bd7
Florencea <bearflorencea@gmail.com>
2024-02-06 14:11:17 +0800

    feat: use prisma uuid

--- prisma/extensions.ts
+++ prisma/extensions.ts
@@ -1,29 +1,5 @@
-import { createId } from "@paralleldrive/cuid2";
 import { Prisma } from "@prisma/client";
 import argon2 from "argon2";
-
-export const cuid2Extension = () => {
-  return Prisma.defineExtension({
-    name: "cuid2",
-    query: {
-      $allModels: {
-        create: async ({ args, query }) => {
-          const id = createId();
-          args.data = { ...args.data, id };
-          return query(args);
-        },
-        upsert: async ({ args, query }) => {
-          const id = createId();
-          args.create = {
-            ...args.create,
-            id,
-          };
-          return query(args);
-        },
-      },
-    },
-  });
-};
 
 export const userCreateExtension = () => {
   return Prisma.defineExtension({
--- prisma/index.ts
+++ prisma/index.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { cuid2Extension, userCreateExtension } from "./extensions";
+import { userCreateExtension } from "./extensions";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ errorFormat: "minimal" })
-    .$extends(cuid2Extension())
-    .$extends(userCreateExtension());
+  return new PrismaClient({ errorFormat: "minimal" }).$extends(
+    userCreateExtension(),
+  );
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
